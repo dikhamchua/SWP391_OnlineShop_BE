c84583b32bdea9b5526040a8e7ec021b5490a68b
foruser <vinhpham2761.gmail.com>
2025-02-25 10:12:31 +0700

    init project

---
+++ src/main/java/org/example/swp391/Swp391OnlineShopBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.swp391;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swp391OnlineShopBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swp391OnlineShopBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/swp391/Swp391OnlineShopBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.swp391;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swp391OnlineShopBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
